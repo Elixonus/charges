2ab7126ce6ad56cd7a71093156712bb599f851f6
Elias <elias@dobrin.net>
2022-01-02 17:53:49 -0800

    using cairo graphics again, got batlow working

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from math import tau
 from copy import copy
 import csv
 import numpy as np
+import cairo
 from points import Point
 from charges import System, PointCharge, ELEMENTARY_CHARGE
 import sources as src
@@ -14,13 +15,13 @@ VIEWPORT_MAXIMUM_X: float = 10.
 VIEWPORT_MAXIMUM_Y: float = 10.
 VIEWPORT_RANGE_X: float = VIEWPORT_MAXIMUM_X - VIEWPORT_MINIMUM_X
 VIEWPORT_RANGE_Y: float = VIEWPORT_MAXIMUM_Y - VIEWPORT_MINIMUM_Y
-IMAGE_LENGTH: int = 100
+IMAGE_LENGTH: int = 400
 IMAGE_AREA: int = IMAGE_LENGTH ** 2
 
-electric_system: System = System(PointCharge(-5 * ELEMENTARY_CHARGE, Point(5, 5)),
-                                 PointCharge(-5 * ELEMENTARY_CHARGE, Point(7, 5)),
-                                 PointCharge(-5 * ELEMENTARY_CHARGE, Point(2, 7)),
-                                 PointCharge(-5 * ELEMENTARY_CHARGE, Point(2, 7)))
+electric_system: System = System(
+    PointCharge(5 * ELEMENTARY_CHARGE, Point(5, 5)),
+    PointCharge(-5 * ELEMENTARY_CHARGE, Point(7, 5)),
+    PointCharge(5 * ELEMENTARY_CHARGE, Point(5, 7)))
 
 ELECTRIC_FIELD_LINE_ITERATION_LIMIT: int = 1000
 ELECTRIC_FIELD_LINE_ITERATION_STEP: float = 0.01
@@ -66,107 +67,35 @@ electric_potentials: list[list[float]] = [[electric_system.potential(Point(
     VIEWPORT_MINIMUM_Y + VIEWPORT_RANGE_Y * (image_y / (IMAGE_LENGTH - 1))
 )) for image_y in range(IMAGE_LENGTH)] for image_x in range(IMAGE_LENGTH)]
 
-electric_potentials_sorted: list[float] = sorted(electric_potential
-                                                 for electric_potentials_buffer in electric_potentials
-                                                 for electric_potential in electric_potentials_buffer)
-electric_potential_low: float = electric_potentials_sorted[round(0.01 * (IMAGE_AREA - 1))]
-electric_potential_high: float = electric_potentials_sorted[round(0.99 * (IMAGE_AREA - 1))]
+electric_potentials_sorted: list[float] = sorted(
+    electric_potential
+    for electric_potentials_buffer in electric_potentials
+    for electric_potential in electric_potentials_buffer)
+electric_potential_low: float = electric_potentials_sorted[round(0.005 * (IMAGE_AREA - 1))]
+electric_potential_high: float = electric_potentials_sorted[round(0.995 * (IMAGE_AREA - 1))]
 electric_equipotentials: list[float] = [
     electric_potentials_sorted[round(percentile * (IMAGE_AREA - 1))] for percentile in
     (0.05, 0.25, 0.5, 0.75, 0.95)
 ]
 
+with open("colors.txt") as file:
+    colors: list[list[float]] = [[float(value) for value in line.split(sep=" ")] for line in
+                                 file.read().split(sep="\n")]
+
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, IMAGE_LENGTH, IMAGE_LENGTH)
+ctx = cairo.Context(surface)
+
+for x in range(IMAGE_LENGTH):
+    for y in range(IMAGE_LENGTH):
+        ctx.rectangle(x, y, 1, 1)
+        normalized_electric_potential = min(max((electric_potentials[x][y] - electric_potential_low) /
+                                                (electric_potential_high - electric_potential_low), 0), 1)
+        ctx.set_source_rgb(*colors[round(255 * normalized_electric_potential)])
+        ctx.fill()
+
+surface.write_to_png("drawing.png")
+
 
 def viewport(point: Point, /) -> tuple[float, float]:
-    return (point.x - VIEWPORT_MINIMUM_X) / VIEWPORT_RANGE_X,\
+    return (point.x - VIEWPORT_MINIMUM_X) / VIEWPORT_RANGE_X, \
            (point.y - VIEWPORT_MINIMUM_Y) / VIEWPORT_RANGE_Y
-
-
-points_x: list[float] = [VIEWPORT_MINIMUM_X + VIEWPORT_RANGE_X * index_x / (IMAGE_LENGTH - 1) for index_x in
-                         range(IMAGE_LENGTH)]
-points_y: list[float] = [VIEWPORT_MINIMUM_Y + VIEWPORT_RANGE_Y * index_y / (IMAGE_LENGTH - 1) for index_y in
-                         range(IMAGE_LENGTH)]
-
-electric_potentials: list[list[float]] = []
-
-for point_x in points_x:
-    electric_potentials_buffer: list[float] = []
-
-    for point_y in points_y:
-        electric_potential: float = electric_system.potential(Point(point_x, point_y))
-        electric_potentials_buffer.append(electric_potential)
-
-    electric_potentials.append(electric_potentials_buffer)
-
-electric_potentials_flattened: list[float] = [electric_potential for electric_potentials_buffer in electric_potentials
-                                              for electric_potential in electric_potentials_buffer]
-electric_potentials_sorted: list[float] = sorted(electric_potentials_flattened)
-electric_equipotential_low: float = electric_potentials_sorted[round(0.01 * (IMAGE_AREA - 1))]
-electric_equipotential_high: float = electric_potentials_sorted[round(0.99 * (IMAGE_AREA - 1))]
-
-electric_potentials_distorted: list[list[float]] = []
-
-for electric_potentials_buffer in electric_potentials:
-    electric_potentials_distorted_buffer: list[float] = []
-
-    for electric_potential in electric_potentials_buffer:
-        electric_potential_distorted: float = min(max((electric_potential - electric_equipotential_low) / (
-                electric_equipotential_high - electric_equipotential_low), 0), 1)
-        electric_potentials_distorted_buffer.append(electric_potential_distorted)
-
-    electric_potentials_distorted.append(electric_potentials_distorted_buffer)
-
-electric_equipotentials: list[float] = [electric_potentials_sorted[round(0.4 * (IMAGE_AREA - 1))]]
-
-for electric_equipotential in electric_equipotentials:
-    electric_potentials_flag: list[list[bool]] = [
-        [electric_potential > electric_equipotential for electric_potential in electric_potentials_buffer] for
-        electric_potentials_buffer in electric_potentials]
-
-    electric_potentials_case: list[list[int]] = []
-
-    for electric_potentials_flag_index, electric_potentials_flag_buffer in enumerate(electric_potentials_flag[:-1]):
-        electric_potentials_case_buffer: list[int] = []
-
-        for electric_potential_flag_index, electric_potential_flag in enumerate(electric_potentials_flag_buffer[:-1]):
-            electric_potential_horizontal_flag: bool = electric_potentials_flag[electric_potentials_flag_index + 1][
-                electric_potential_flag_index]
-            electric_potential_vertical_flag: bool = electric_potentials_flag[electric_potentials_flag_index][
-                electric_potential_flag_index + 1]
-            electric_potential_diagonal_flag: bool = electric_potentials_flag[electric_potentials_flag_index + 1][
-                electric_potential_flag_index + 1]
-            electric_potential_case: int = 0
-
-            if electric_potential_flag:
-                electric_potential_case += 8
-
-            if electric_potential_horizontal_flag:
-                electric_potential_case += 4
-
-            if electric_potential_diagonal_flag:
-                electric_potential_case += 2
-
-            if electric_potential_vertical_flag:
-                electric_potential_case += 1
-
-            electric_potentials_case_buffer.append(electric_potential_case)
-
-        electric_potentials_case.append(electric_potentials_case_buffer)
-
-with open("color_mapping.csv") as file:
-    color_mapping: list[list[float]] = list(csv.reader(file, quoting=csv.QUOTE_NONNUMERIC))
-
-image: list[list[list[float]]] = []
-
-for electric_potentials_distorted_buffer in electric_potentials_distorted:
-    image_buffer: list[list[float]] = []
-
-    for electric_potential_distorted in electric_potentials_distorted_buffer:
-        pixel: list[float] = [round(255 * channel) for channel in
-                              color_mapping[round(255 * electric_potential_distorted)]]
-        image_buffer.append(pixel)
-
-    image.append(image_buffer)
-
-# a = Image.fromarray(np.array(image).astype(np.uint8))
-# a.show(a)
